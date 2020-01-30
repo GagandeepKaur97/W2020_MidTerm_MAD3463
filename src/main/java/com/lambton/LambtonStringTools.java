015a7ca9fc5b77f0cb3c8572696b1e83fccdf135
gagandeep kaur <sherry2312.gk@gmail.com>
2020-01-29 19:47:40 -0500

    gagandee

--- src/main/java/com/lambton/LambtonStringTools.java
+++ src/main/java/com/lambton/LambtonStringTools.java
@@ -87,7 +87,7 @@ public class LambtonStringTools
     public static String replaceSubString(String originalString, String findString, String newString) 
     {
         String str[]= new String[ "Lambton College in Toronto"];
-        System.out.println(str.replacefirst("college","university"));
+        System.out.println(replaceSubString.toString("college","university"));
 
         return null;
     }
